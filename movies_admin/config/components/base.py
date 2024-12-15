536bd610d38189ca10d6420db7863a037edb77c2
mikheyev9 <mikheyev9@gmail.com>
2024-12-15 16:02:30 +0300

    feat: Add support for Django Debug Toolbar in Docker container Note: Debug Toolbar will not function when accessed through Nginx proxy

--- movies_admin/config/components/base.py
+++ movies_admin/config/components/base.py
@@ -6,9 +6,12 @@ SECRET_KEY = os.environ.get('SECRET_KEY', ''),
 
 DEBUG = os.environ.get('DEBUG', False) == 'True'
 
-
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 INTERNAL_IPS = os.getenv('INTERNAL_IPS').split(',')
+if DEBUG:
+    import socket
+    hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+    INTERNAL_IPS += [ip[: ip.rfind('.')] + '.1' for ip in ips]
 
 ROOT_URLCONF = 'config.urls'
 
